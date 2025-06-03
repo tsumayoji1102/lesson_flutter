d6ff1359261af5ab8a54bf47edebd84e4e209913
tsumayoji1102 <ryosukesukairain1@gmail.com>
2025-06-04 00:34:27 +0900

    lesson_4追加

---
+++ lib/lesson/lesson_4.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Lesson4Widget extends StatefulWidget {
+  const Lesson4Widget({super.key});
+
+  /// ウィジェットが作られるときに最初に1回だけ呼ばれる.
+  ///
+  /// Lesson4Widgetは、_Lesson4WidgetStateを状態として持つ、という意味。
+  @override
+  State<Lesson4Widget> createState() => _Lesson4WidgetState();
+}
+
+class _Lesson4WidgetState extends State<Lesson4Widget> {
+  int _count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("タイトル"), backgroundColor: Colors.lightGreen),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$_count', style: TextStyle(fontSize: 20)),
+            ElevatedButton(
+              /// ボタンをタップすると、_incrementが呼ばれる。
+              onPressed: _increment,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.lightGreen,
+              ),
+              child: const Text('+1', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// 数字を1増やす。そして、画面を再レンダリングする。
+  void _increment() {
+    _count++;
+    setState(() {});
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:lesson_flutter/lesson/lesson_3.dart';
+// import 'package:lesson_flutter/lesson/lesson_3.dart';
+import 'package:lesson_flutter/lesson/lesson_4.dart';
 
 /// アプリ起動はここから行われる。
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
       ),
-      home: const PracticeLayout(),
+      home: const Lesson4Widget(),
     );
   }
 }
